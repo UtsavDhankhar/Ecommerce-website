50bebcc9c808ca62ff3e2ba8e97497b49bfd1696
Utsav Dhankhar <utsav900@gmail.com>
2022-08-26 22:12:18 +0530

    fixed issue with mongodb

--- models/product.js
+++ models/product.js
@@ -2,7 +2,7 @@ var express=require('express');
 var mongoose=require('mongoose');
 var mongoosePaginate=require('mongoose-paginate');
 var Schema = mongoose.Schema
-mongoose.connect('mongodb+srv://Utsav:Live2DiE@cluster0.9xtw8o5.mongodb.net/?retryWrites=true&w=majority',{ useUnifiedTopology: true, useNewUrlParser: true  })
+mongoose.connect('mongodb+srv://Utsav:<password>@cluster0.9xtw8o5.mongodb.net/?retryWrites=true&w=majority',{ useUnifiedTopology: true, useNewUrlParser: true  })
 
 const ProductSchema=new Schema({
 
